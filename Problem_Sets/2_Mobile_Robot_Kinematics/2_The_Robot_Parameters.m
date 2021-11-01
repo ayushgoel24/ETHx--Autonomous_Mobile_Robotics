7ea868bde8bc825060e534e32b03b0503dcc37b4
Ayush Goel <ayush.goel2427@gmail.com>
2021-11-01 15:34:56 +0000

    added second module

---
+++ Problem_Sets/2_Mobile_Robot_Kinematics/2_The_Robot_Parameters.m
@@ -0,0 +1,19 @@
+% There are several possible answers to this so the check
+% is based on the stacked wheel equations.
+
+% Wheel 1, the far right wheel
+alpha1 = 0;
+beta1 = 0;
+ell1 = 0.5;
+
+
+% Wheel 2, the top left wheel
+alpha2 = 2*pi/3;
+beta2 = 0;
+ell2 = 0.5;
+      
+
+% Wheel 3, the bottom left wheel
+alpha3 = 4*pi / 3;
+beta3 = 0;
+ell3 = 0.5;
